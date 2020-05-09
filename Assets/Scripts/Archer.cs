8b26c9bb156ca891f51d1771681d8254980b46d2
Серёжа Серёжа <you@example.com>
2020-05-09 13:01:36 +0300

    что-то непонятное, из чего потом получится игра(наверное)

---
+++ Assets/Scripts/Archer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    public float speed;
+    public FloatingJoystick variableJoystick;
+    public Rigidbody rb;
+    public Vector3 kek;
+
+    public void FixedUpdate()
+    {
+        
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        this.kek = variableJoystick.Direction;
+    }
+}
