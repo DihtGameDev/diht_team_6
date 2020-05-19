05e862b119b3f1d0ff0231f44c2297f581688d29
Серёжа Серёжа <you@example.com>
2020-05-19 18:35:48 +0300

    реализовал стрельбу по нажатию на экран

---
+++ Assets/Scripts/archer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class archer : MonoBehaviour
+{
+    [SerializeField] private Joystick joystick;
+    [SerializeField] private float speed = 30f;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 velocity;
+    private float arrowSpeed = 50f;
+    public Vector3 direction;
+    public Vector3 mousePos;
+    public GameObject arrow;
+    
+
+    void FixedUpdate()
+    {
+        velocity = new Vector2(joystick.Horizontal * speed, joystick.Vertical * speed);
+        rb.velocity = velocity;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var distanceToCamera = transform.position.z - cam.transform.position.z;
+            mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToCamera);
+            direction = cam.ScreenToWorldPoint(mousePos) - transform.position;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var from = transform.position;
+        var ar = Instantiate(arrow, from, transform.rotation);
+        var arrowRb = ar.GetComponent<Rigidbody2D>();
+        arrowRb.velocity = direction.normalized * arrowSpeed;
+        var angle = Vector3.Angle(Vector3.right, direction);
+        if(direction.y < 0)
+        {
+            angle = -1 * angle;
+        }
+        arrowRb.transform.Rotate(Vector3.forward, angle);
+    }
+}
---
+++ Assets/Scripts/arrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name != "archer")
+        {
+            Debug.Log("name = " + collision.name);
+            var box = collision.GetComponent<box>();
+            if (box != null)
+            {
+                box.TakeDamage(1);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+}
