bd234a5fe71be57facb23ba903fed8abc1785c5b
Серёжа Серёжа <you@example.com>
2020-05-21 15:06:14 +0300

    доработал стрельбу. Сделал прицеливание. Добавил дальность стрельбы. Джойтик больше не ломает стрельбу

--- Assets/Scripts/arrow.cs
+++ Assets/Scripts/arrow.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class arrow : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private float attackRange;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.name != "archer")
         {
-            Debug.Log("name = " + collision.name);
             var box = collision.GetComponent<box>();
             if (box != null)
             {
                 box.TakeDamage(1);
             }
+            Destroy(gameObject);
+        }
+    }
+
+    public void setStartPosition(Vector3 pos)
+    {
+        startPosition = pos;
+    }
+
+    public void setAttackRange(float range)
+    {
+        attackRange = range;
+    }
+
+    private void Update()
+    {
+        if((startPosition - transform.position).magnitude > attackRange)
+        {
             Destroy(gameObject);
         }
     }
