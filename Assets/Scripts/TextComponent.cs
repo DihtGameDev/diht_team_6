d91fde09594d6ba9908efe84a4607a3331b16e1f
Your Name <you@example.com>
2020-05-22 20:01:04 +0300

    добавил меню страницу, локализацию, и еще мелкие поправки :)

---
+++ Assets/Scripts/TextComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextComponent : MonoBehaviour
+{
+
+    [SerializeField] private string textRu;
+    [SerializeField] private string textEn;
+
+    void Start()
+    {
+        var textObj = GetComponent<Text>();
+        var lang = PlayerPrefs.GetString("Language");
+        Debug.Log("lang " + lang);
+        if(lang == "En")
+        {
+            textObj.text = textEn;
+        }
+        else
+        {
+            textObj.text = textRu;
+        }
+    }
+
+}
